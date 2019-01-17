77a81606859997182192f3b89c1a953b8eb3802f
Marcin Mazur <marcin.mazur1024@gmail.com>
2019-01-17 12:36:12 +0100

    Change Task1.class for the second time

--- src/pl/mazurmarcin/javastart/lecture1/Task1.java
+++ src/pl/mazurmarcin/javastart/lecture1/Task1.java
@@ -7,5 +7,7 @@ public class Task1 {
         System.out.println("Zadanie nr 1");
         System.out.println("Uczę \nsię \nprogramowania \ni \ncommitowania");
 
+        System.out.println("Kolejny przypadkowy tekst");
+
     }
 }
