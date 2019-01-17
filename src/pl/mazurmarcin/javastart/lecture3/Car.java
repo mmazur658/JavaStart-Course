e7f4763c4716d282a42c6fcc4379d1ce43a96d4a
Marcin Mazur <marcin.mazur1024@gmail.com>
2019-01-17 14:37:14 +0100

    Lecture 3 Task 1 Step 1

---
+++ src/pl/mazurmarcin/javastart/lecture3/Car.java
@@ -0,0 +1,22 @@
+package pl.mazurmarcin.javastart.lecture3;
+
+public class Car {
+
+    String brand;
+    String model;
+    int year;
+    double capacity;
+
+    public Car() {
+
+    }
+
+    public Car(String brand, String model, int year, double capacity) {
+        this.brand=brand;
+        this.model=model;
+        this.year=year;
+        this.capacity=capacity;
+
+    }
+
+}
