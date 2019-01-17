b8e859805ca2b975fa25b44f9a3cee6169f85b95
Marcin Mazur <marcin.mazur1024@gmail.com>
2019-01-17 12:35:46 +0100

    Change Task1.class for the second time

--- src/pl/mazurmarcin/javastart/lecture1/Task1.java
+++ src/pl/mazurmarcin/javastart/lecture1/Task1.java
@@ -4,6 +4,7 @@ public class Task1 {
 
     public static void main(String[] args) {
 
+        System.out.println("Zadanie nr 1");
         System.out.println("Uczę \nsię \nprogramowania \ni \ncommitowania");
 
     }
