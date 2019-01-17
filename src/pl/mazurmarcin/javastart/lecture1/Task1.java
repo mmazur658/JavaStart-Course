8a206b100cb78e651633fd48636ad05546d8b30b
Marcin Mazur <marcin.mazur1024@gmail.com>
2019-01-17 12:30:24 +0100

    Add Task1 class

---
+++ src/pl/mazurmarcin/javastart/lecture1/Task1.java
@@ -0,0 +1,10 @@
+package pl.mazurmarcin.javastart.lecture1;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Uczę \nsię \nprogramowania");
+
+    }
+}
